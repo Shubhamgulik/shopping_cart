4a264935ca4e070e109ac2ffebda4ab284ea96ce
Shubhamgulik <42814032+Shubhamgulik@users.noreply.github.com>
2023-02-18 21:54:38 +0530

    Added state for carItem component

---
+++ src/CartItem.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price : 999,
+            title : 'Phone',
+            qty : 1,
+            img : ''
+        }
+    }
+
+    increaseQuantity(){
+        console.log(this.state.qty);
+        this.state.qty += 1; 
+        this.render();
+        return;
+    }
+
+    render(){
+        const {price,title,qty} = this.state;
+        return(
+            <div className="cart-item" >
+                <div className="left-block">
+                    <img style={styles.image} />
+                </div>
+                <div className="right-block">
+                    <div style={ {fontSize: 25} } >{title}</div>
+                    <div style={ {color : '#777'} } >Rs. {price}</div>
+                    <div style={ {color : '#777'} } >Qty: {qty}</div>
+                    <div className="cart-item-actions">
+                        {/* Buttons */}
+                        <img
+                            className="action-icons"
+                            alt="increase" 
+                            src="https://cdn-icons-png.flaticon.com/512/992/992651.png" 
+                            onClick={this.increaseQuantity.bind(this)}
+                        />
+                        <img 
+                            className="action-icons" 
+                            alt="decrease" 
+                            src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
+                        
+                        />
+                        <img
+                            className="action-icons" 
+                            alt="delete" 
+                            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" 
+                         
+                         />
+
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+const styles = {
+        image : {
+            height : 110,
+            width : 110,
+            borderRadius : 4,
+            background: '#ccc'
+
+        }
+}
+
+export default CartItem;
