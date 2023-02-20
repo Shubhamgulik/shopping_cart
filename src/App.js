11a1e8d2e2114308986e8cbf55f4b2ad17608a78
Shubhamgulik <42814032+Shubhamgulik@users.noreply.github.com>
2023-02-20 17:00:06 +0530

    Added navbar.js file with styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import CartItem from './CartItem';
 import Cart from './Cart';
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Shopping Cart</h1>
       < Cart />
       {/* <CartItem /> */}
